444b626deeeb93b23e3f32858a9859179b99970b
Leo Le <lnle125@gmail.com>
2020-05-02 19:32:48 -0700

    completed added

--- index.js
+++ index.js
@@ -1,57 +1,76 @@
-const Choice = require("inquirer/lib/objects/choice");
-
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown")
+const fs = require("fs")
 const questions = [
     {
+        name:  "github",
+        message: "What is your Github username?"
+    },
+    {
+        name: "email",
+        message: "what is your email?"
+    },
+    {
+        name: "projURL",
+        message: "the URL to your project: "
+    },
+
+    {
         name: "title",
-        message: "What is the project title? "
+        message: "What is the project name? "
     },
     {
         name: "description",
         message: "Description of the project: "
     }, 
     {
-        name: "table", 
-        message: "Table of content: "
-    }, 
-    {
-        name: "installation",
-        message: "installation"
-    },
-    {
-        name: "usage",
-        message: "usage"
-    },
-    {
-        input: "checkbox",
+        type: "list",
         name: "license", 
-        message: "Under what license",
-        choices: [ "Appache",
-            "BSD",
-            "GNU",
-            "ISC",
+        // message: "Under what license",
+        choices: [ 
             "MIT",
-            "Mozilla"
+            "Appache 2.0",
+            "GPL 3.0",
+            "BSD 3", 
+            "None"
         ]
     },
     {
-        name: "contribution",
-        message: "Who are the contributors? "
+        name: "installCommand",
+        message: "What command should you run to install dependencies?"
     },
     {
-        name: "tests",
-        message: ""
-        
-    }
+        name: "testCommand",
+        message: "What command should you run to tests?"
+    },    
+    {
+        name: "repoInfo",
+        message: "what user need to know about using your repo?"
     
+    }, 
+    {
+        name: "contribution",
+        message: "what user need to know about contributing to the repo?"
+    }
 
 ];
 
 function writeToFile(fileName, data) {
-
+    fs.writeFile(fileName, data, err => {
+        if (err) return console.log("err")
+        console.log("success!");
+    })
 }
 
-function init() {
-
+init = async () => {
+    let answers;
+    try {
+        answers = await inquirer.prompt(questions);
+        console.log(answers);
+    } catch (err) {
+        console.log(err, "error in prompt");
+    }   
+    writeToFile("README.md", generateMarkdown(answers));
 }
 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,54 @@
-function generateMarkdown(data) {
-  return `
-# ${data.title}
 
+const licenses = {
+    "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    "GPL 3.0":  "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    "Appache 2.0": "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    "BSD 3": "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+    "None": "License: None"
+}
+
+function tOc () {
+    return `
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [License](#License) 
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions) 
+`
+}
+
+
+function generateMarkdown(data) {
+  
+    let tableOfContents = tOc();
+    return `\
+# ${data.title}
+${licenses[data.license]}
+    
+## Description
+    ${data.description}
+
+## Table of Contents 
+    ${tableOfContents}
+
+## Installation
+    ${data.installCommand}
+
+## Usage
+    ${data.repoInfo}
+
+## License
+    This project is licensed under the ${data.license};
+
+## Contributing
+    ${data.contribution}
+
+## Tests
+    ${data.testCommand}
+
+## Questions
+    If you have any questions about the repo, open an issue or contact [${data.github}](${data.email}) at ${data.projURL}.
 `;
 }
 
